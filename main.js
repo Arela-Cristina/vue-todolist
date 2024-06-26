784390bbb759a6c3abc5829304e5b6f4560ce92f
Arela-Cristina <instagrandine@gmail.com>
2024-06-26 20:10:26 +0200

    New object:
    newTask whit properties text and done.
    
    Clone this object to add always a new Task in to the array.

--- main.js
+++ main.js
@@ -4,23 +4,49 @@ createApp({
     data() {
         return {
             logo: './img/logo.jpg',
-            // aggiungiamo un collegamento tra data ed il campo di imput 
-            newTask: '',
+            // aggiungiamo un collegamento tra data ed il campo di imput, il collegamento dovra essere un obbieto che uguale agli altri ha la proprieta text e la proprieta done.
+            input: '',
             tasks: [
-                'Allenarmi',
-                'Studiare',
-                'Fare il bucato',
-                'Fare la Spessa',
-                'Prendere la lettiera del gato',
+                {
+                    text: 'Allenarmi',
+                    done: true,
+                },
+                {
+                    text: 'Studiare',
+                    done: true,
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false,
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: false,
+                },
+                {
+                    text: 'Prendere la Lettiera del Gatto',
+                    done: true,
+                },
             ],
+            newTask: {
+                text: '',
+                done: '',
+            },
+
         }
     },
     methods: {
 
         // aggiungiamo il evento del click al bottone Aggiungi
         addTask() {
-            // sempre utilizziamo 'this' per richiamare una proprieta. unshift e simile  push, cioe pusha, ma il elemento si posizziona nel indice 0.
-            this.tasks.unshift(this.newTask);
+
+            //creiamo una ugualianza tra  il v-model del input e la proprieta text del oggeto newTask, e facciamo new.task.text perche sto richiamando a la proprieta di un oggeto.
+            this.newTask.text = this.input;
+            //facciamo un clone del oggeto newTask, cosi che possiamo aggiungere diverse task
+            let cloneNewTask = { ...this.newTask };
+            // sempre utilizziamo 'this' per richiamare una proprieta. Unshift e simile a  push, cioe pusha, ma il elemento si posizziona nel indice 0.
+            this.tasks.unshift(cloneNewTask);
+            console.log('hey', this.newTask);
         },
 
         // aggiungiamo il evento del click a le 'X' per cancellare task
