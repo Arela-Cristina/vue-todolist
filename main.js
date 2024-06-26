2a1ff0b38508ac446d892655d66cdbf5688ffb96
Arela-Cristina <instagrandine@gmail.com>
2024-06-26 17:20:58 +0200

    Added to methods remove task:
    then created the argument eachIndex, and used the splice property, to remove elements from array tasks

--- main.js
+++ main.js
@@ -21,6 +21,13 @@ createApp({
         addTask() {
             // sempre utilizziamo 'this' per richiamare una proprieta. unshift e simile  push, cioe pusha, ma il elemento si posizziona nel indice 0.
             this.tasks.unshift(this.newTask);
+        },
+
+        // aggiungiamo il evento del click a le 'X' per cancellare task
+        removeTask(eachIndex) {
+            console.log(eachIndex);
+            //perrimuovere un elemento especifico del array utilizziamo splice, e li passiamo il argomento eachIndex, e anche il uno,perche vogliamo che al click della X ci cancella soltanto un singolo elemento.
+            this.tasks.splice(eachIndex, 1);
         }
     }
 }
