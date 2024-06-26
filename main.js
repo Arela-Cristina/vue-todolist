8286f65b0fb4a8830b9e5c6a2b7fc99af6bcee40
Arela-Cristina <instagrandine@gmail.com>
2024-06-26 14:29:07 +0200

    Added main.js vue builder structure

---
+++ main.js
@@ -0,0 +1,38 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            logo: 0,
+            tasks: [
+                'Allenarmi',
+                'Studiare',
+                'Fare il bucato',
+                'Fare la Spessa',
+                'Prendere la lettiera del gato',
+            ],
+        }
+    },
+    methods: {
+
+        // nextImage() {
+        //     console.log('hey');
+
+        //     if (this.activeItem >= this.slides.length - 1) {
+        //         this.activeItem = 0;
+        //     } else {
+        //         this.activeItem++;
+        //     }
+        // },
+
+        // prevImage() {
+
+        //     if (this.activeItem <= 0) {
+        //         this.activeItem = this.slides.length - 1;
+        //     } else {
+        //         this.activeItem--;
+        //     }
+        // }
+    }
+}
+).mount('#app') 
